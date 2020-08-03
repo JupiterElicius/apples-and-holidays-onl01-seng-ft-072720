e1f07d1dd6fca1e20371669e6a2b26df5c36b98f
Nick Bazaldua <bazalnick@gmail.com>
2020-08-03 09:17:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,62 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 end
 
 
+def add_supply_to_memorial_day(holiday_hash, supply)
+  # again, holiday_hash is the same as the ones above
+  # add the second argument to the memorial day array
+  
+  holiday_hash.each do |holiday, sup|
+    if holiday == :spring
+      sup.each do |attribute, data|
+        data << supply
+      end
+    end
+  end
+  holiday_hash
 
+end
+
+def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  holiday_hash[season][holiday_name] = supply_array
+  
+  holiday_hash
+end
+
+def all_winter_holiday_supplies(holiday_hash)
+  holiday_hash[:winter].values.flatten
+end
+
+def all_supplies_in_holidays(holiday_hash)
+  # iterate through holiday_hash and print items such that your readout resembles:
+  # Winter:
+  #   Christmas: Lights, Wreath
+  #   New Years: Party Hats
+  # Summer:
+  #   Fourth Of July: Fireworks, BBQ
+  # etc.
+  
+  #season, key, and value
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, item|
+      puts "  #{holiday.to_s.split("_").map {|v| v.capitalize}.join(" ")}: #{item.join(", ")}"
+    end
+  end
+end
+[2:17 AM]
+def all_holidays_with_bbq(holiday_hash)
+  # return an array of holiday names (as symbols) where supply lists
+  # include the string "BBQ"
+  
+  #
+  
+  holiday_hash.map do |season, holiday| 
+    holiday.map do |holiday, item|
+     holiday if item.include?("BBQ")#returning holiday if condition is met 
+    end
+  end
+  .flatten.compact #turns results into one array and removes nil using compact
+end
 
 
 
